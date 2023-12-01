30dbd84677d929a006e711cc333f1d67d970dd2f
kasdiana25 <kasumova.dianka@yandex.ru>
2023-12-01 19:04:16 +0700

    rabota 1.3

---
+++ rabota 1.3/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задание 1
+ string[] lines = File.ReadAllLines("input.txt");
+string[] lucky = lines[0].Split(' ');
+int tiket = Convert.ToInt32(lines[1]);
+StreamWriter writer = new StreamWriter("output.txt", true);
+for (int i = 2; i < 2 + tiket; i++)
+{
+    int a = 0;
+    string[] Num = lines[i].Split(' ');
+    foreach (var n in Num)
+    {
+        if (lucky.Contains(n))
+        {
+            a++;
+        }
+    }
+
+    if (a >= 3)
+    {
+        writer.WriteLine("Lucky");
+        Console.WriteLine("Lucky");
+    }
+    else
+    {   
+        writer.WriteLine("Unlucky");
+        Console.WriteLine("Unlucky");
+    }
+}
+writer.Close();
+ */
+
+string[] num = File.ReadAllLines("nums.txt");
+string[] nums = num[0].Split(' ');
+List<string> b = new List<string>();
+foreach (var n in nums)
+{
+    if (Convert.ToInt32(n) % 2 != 0)
+    {
+        b.Add(n);
+    }
+}
+
+string number = " ";
+foreach (var n in b)
+{
+    number += $"{n} ";
+}
+
+StreamWriter writer = new StreamWriter("nums.txt", false);
+writer.Write(number);
+writer.Close();
+
--- rabota_1.2/Program.cs
+++ rabota_1.2/Program.cs
@@ -80,3 +80,148 @@ public class Program
     }
 }
  */
+
+/* Задание 4
+ static double[] AvgTemp(int[,] temps)
+ {
+    double[] avgMonthTemp = new double[12];
+    float sum = 0;
+
+    for (int i = 0; i < 12; i++)
+    {
+        sum = 0;
+        for (int j = 0; j < 30; j++)
+        {
+            sum = sum + temps[i, j];
+        }
+        avgMonthTemp[i] = Math.Round(sum / 30, 1);
+    }
+
+    return avgMonthTemp;
+}
+
+int[,] temperature = new int[12, 30];
+int min = -30;
+int max = -20;
+int x = 8;
+Random rand = new Random();
+
+for (int i = 0; i < 12; i++)
+{
+    for (int j = 0; j < 30; j++)
+    {
+        temperature[i, j] = rand.Next(min, max);
+    }
+   
+    if (i<6)
+    {
+        min = min + x;
+        max = max + x;
+    }
+    else 
+    {
+        min = min - x;
+        max = max - x;
+    }
+}
+
+double[] avg = AvgTemp(temperature);
+
+Array.Sort(avg);
+
+foreach (var m in avg)
+{
+    Console.WriteLine(m);
+}
+ */
+ 
+ /* Задание 5
+static Dictionary<string, double> AvgTemp(Dictionary<string, int[]> temps)
+  
+{
+    Dictionary<string, double> avgMonthTemp = new Dictionary<string, double>()
+    {
+        { "Январь", 0 },
+        { "Февраль", 0 },
+        { "Март", 0 },
+        { "Апрель", 0 },
+        { "Май", 0 },
+        { "Июнь", 0 },
+        { "Июль", 0 },
+        { "Август", 0 },
+        { "Сентябрь", 0 },
+        { "Октябрь", 0 },
+        { "Ноябрь", 0 },
+        { "Декабрь", 0 }
+    };
+    float sum = 0;
+
+    int nm = 0;
+    foreach (var month in temps)
+    {
+        sum = 0;
+        for (int i = 0; i < 30; i++)
+        {
+            sum = sum + temps[month.Key][i];
+        }
+        avgMonthTemp[month.Key] = Math.Round(sum / 30, 1);
+        nm++;
+    }
+
+    return avgMonthTemp;
+}
+
+Dictionary<string, int[]> temperature = new Dictionary<string, int[]>()
+{
+    {"Январь", new int[30]},
+    {"Февраль", new int[30]},
+    {"Март", new int[30]},
+    {"Апрель", new int[30]},
+    {"Май", new int[30]},
+    {"Июнь", new int[30]},
+    {"Июль", new int[30]},
+    {"Август", new int[30]},
+    {"Сентябрь", new int[30]},
+    {"Октябрь", new int[30]},
+    {"Ноябрь", new int[30]},
+    {"Декабрь", new int[30]}
+};
+
+int min = -30;
+int max = -20;
+int x = 8;
+int nm = 0;
+Random rand = new Random();
+
+foreach (var month in temperature)
+{
+    for (int i = 0; i < 30; i++)
+    {
+        temperature[month.Key][i] = rand.Next(min, max);
+    }
+   
+    if (nm<6)
+    {
+        min = min + x;
+        max = max + x;
+    }
+    else 
+    {
+        min = min - x;
+        max = max - x;
+    }
+
+    nm++;
+}
+
+Dictionary<string, double> avgTemp = AvgTemp(temperature);
+
+foreach (var m in avgTemp)
+{
+    Console.Write(m.Key + ": ");
+    Console.WriteLine(m.Value);
+}
+  
+  
+  
+ */
