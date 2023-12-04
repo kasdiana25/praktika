f6cf894286134cb898fe599e786f7084e5c8956a
Касумова Диана Парвизовна <gr622_kadpa@ttit.local>
2023-12-04 15:02:20 +0700

    rabota 1.4

---
+++ rabota 1.4/Program.cs
@@ -0,0 +1,98 @@
+﻿/* Задание 1
+ Console.WriteLine("Введите  целое положительное число n: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+int a = 1; 
+for (int i = 3; i < n; i++)
+{
+    if (i % 3 == 0)
+    {
+        a *= i;
+    }
+}
+Console.WriteLine($"Произведение равно {a}");
+*/
+
+/* Задание 2
+public class Mainc
+{
+public static void Main()
+{
+    string[] num = File.ReadAllLines("numsTask2.txt");
+    string[] nums = num[0].Split(';');
+    float sum = 0;
+    foreach (var n in nums)
+    {
+        if (Convert.ToSingle(n) == 0)
+        {
+            break;
+        }
+        if (Convert.ToSingle(n) > 0)
+        {
+            sum += Convert.ToSingle(n);
+        }
+    }
+    Console.WriteLine($"Сумма положительный чисел: {sum}");
+}
+}
+ */
+
+/* Задание 3
+ 
+ public class Mainc
+ {
+    public static void Main()
+    {
+        string[] num = File.ReadAllLines("numsTask3.txt");
+        string[] nums = num[0].Split(',');
+        int max = -100;
+        int min = 100;
+        foreach (var n in nums)
+        {
+            if (Convert.ToInt32(n) == 0)
+            {
+                break;
+            }
+            if (Convert.ToInt32(n) > max)
+            {
+                max = Convert.ToInt32(n);
+            }
+            if (Convert.ToInt32(n) < min)
+            {
+                min = Convert.ToInt32(n);
+            }
+        
+        }
+        double a = Convert.ToDouble(min) / Convert.ToDouble(max);
+        double b = Convert.ToDouble(max) / Convert.ToDouble(min);
+        Console.WriteLine($"Отношение минимального к максимальному: {a}");
+        Console.WriteLine($"Отношение максимального к минимальному: {b}");
+    }
+}
+ */
+
+public class Mainc
+{
+public static void Main()
+{
+    string[] num = File.ReadAllLines("numsTask3.txt");
+    string[] nums = num[0].Split(',');
+int 
+    
+/* Задание 5
+ Console.WriteLine("Введите координаты точек a, b: ");
+ float a = Convert.ToSingle(Console.ReadLine());
+float b = Convert.ToSingle(Console.ReadLine());
+float x1 = Convert.ToSingle(3);
+float x2 = Convert.ToSingle(-1);
+float y1 = Convert.ToSingle(4);
+float y2 = Convert.ToSingle(-2);
+if ((a >= x2 && a <= x1) && (b >= y2 && b <= y1))
+{
+    Console.WriteLine($"Точк c принадлежит заштрихованной области");
+}
+else
+{
+    Console.WriteLine($"Точка c не принадлежит заштрихованной области");
+}
+*/
+
